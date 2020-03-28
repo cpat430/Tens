bd6498be1e4ab12ed5ad92f7d2e6594dcc5c1122
sammochen <samuelshchen@gmail.com>
2020-03-29 09:31:39 +1300

    Support multi-player role-like turns

--- server.js
+++ server.js
@@ -12,39 +12,38 @@ app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+server.listen(5000, function () {
+    console.log('Starting server on port 5000');
 });
 
-var players = {};
-io.on('connection', function(socket) {
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 300,
-      y: 300
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
+let total_players = 0;
+let current_player = 1; // 1-indexed
+
+io.on('connection', function (socket) {
+    
+    let id = -1;
+    // on each new player connection, these functions are called
+    socket.on('new player', function () {
+        total_players++;
+        console.log("This is ID " + total_players);
+        id = total_players;
+
+        socket.emit('init', id);
+    });
+
+    socket.on('turn', function () {
+        // update game state
+        if (id == current_player) current_player = current_player >= total_players ? 1 : current_player + 1;
+    });
+
+    // it does not handle disconnect
 });
 
-setInterval(function() {
-  io.sockets.emit('state', players);
+
+setInterval(function () {
+    io.sockets.emit('state', current_player + "/" + total_players); 
 }, 1000 / 60);
--- static/client.js
+++ static/client.js
@@ -1,61 +1,30 @@
+// keep this!
 var socket = io();
 
-var movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
-}
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
-});
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
-});
-
 socket.emit('new player');
-setInterval(function() {
-  socket.emit('movement', movement);
-}, 1000 / 60);
 
 var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-  //console.log(players);
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
-  for (var id in players) {
-    var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
+
+canvas.onclick=move;
+
+function move(e) {
+    socket.emit('turn'); // this is the way of having a turn
+}
+
+socket.on('init', function(state) {
+    let paragraph = document.getElementById('state');
+    paragraph.innerHTML = "You are person " + state;
+
 });
+
+socket.on('state', function (state) {
+    showState(state);
+});
+
+function showState(state) {
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, 800, 600);
+    ctx.fillText(state, 400, 300);
+}
